7a661d78ab4299f11f7dfb3421c212ae804e8fa6
Jason Kim <jasonkim4201@gmail.com>
2019-01-10 20:44:12 -0500

    attempted to make ajax on nutrition work

--- assets/logic.js
+++ assets/logic.js
@@ -1,15 +1,24 @@
 
-
-
-
-
-
-
-
 //Search button sumbit function
-$("#searchBtn").on("click", function(event) {
+$("#searchBTN").on("click", function(event) {
   event.preventDefault();
   console.log("YOU CLICKED ME")
+ 
   var searchInput = $("#beerInput").val().trim();
   console.log(searchInput);
+  var queryUrl = "https://trackapi.nutritionix.com/v2/search/item?" + searchInput;
+
+
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+    headers: {
+      "x-app-id": "87764d56",
+      "x-app-key": "64b0113675aca1dbf6f67d9df8299556"
+    }
+  }).then(function(response) {
+    console.log(response);
+  });
+
+
 });
